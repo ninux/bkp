c807f2066e9836f90ee1eed94e2ad66a036551d3
Nino Ninux <nino.ninux@gmail.com>
2014-11-23 11:46:17 +0100

    added some input checks

--- src/backup.sh
+++ src/backup.sh
@@ -2,18 +2,54 @@
 
 BASE=/home/$USER/
 DIR=$1
-PREFIX="bkp"
-DEVICE="$HOSTNAME"
-NOW=$(date +%Y-%m-%d)
-EXTENSION="zip"
-FILE=$PREFIX"_"$DEVICE"_"$DIR"_"$NOW"."$EXTENSION
 
-{
-	zip -r $FILE $BASE$DIR
-} &> /dev/null
+# check for no-argument
+if [ $# -eq 0 ]
+then
+	echo "no argument given (see help)"
+	exit 1
+fi
 
-DIV=1024
-FILESIZE=$(stat -c%s "$FILE")
-let "SIZE = $FILESIZE / $DIV"
+# check for null-string
+if [ -z "$1" ]
+then
+	echo "no argument given (see help)"
+	exit 1
+fi
 
-echo "created $FILE ($SIZE MB)"
+# check for help
+if [ $1 == "help" ]
+then
+	echo "type the directory name from $BASE you want to backup"
+	exit 1
+fi
+
+# check for exact number of arguments
+if [ $# -ne 1 ]
+then
+	echo "wrong number of arguments (see help)"
+	exit 1
+fi
+
+# check if directory exists
+if [ ! -d "$BASE$DIR" ]
+then
+	echo "given directory '$BASE$DIR' is invalid"
+	exit 1
+else
+	PREFIX="bkp"
+	DEVICE="$HOSTNAME"
+	NOW=$(date +%Y-%m-%d)
+	EXTENSION="zip"
+	FILE=$PREFIX"_"$DEVICE"_"$DIR"_"$NOW"."$EXTENSION
+	
+	{
+		zip -r $FILE $BASE$DIR
+	} &> /dev/null
+	
+	DIV=1024
+	FILESIZE=$(stat -c%s "$FILE")
+	let "SIZE = $FILESIZE / $DIV"
+	
+	echo "created $FILE ($SIZE MB)"
+fi
